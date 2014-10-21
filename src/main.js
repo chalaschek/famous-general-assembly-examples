ed4bf50a5860d0adeae3e5f6f643fd1281a428ed
chalaschek <chalaschek@gmail.com>
2014-10-21 17:17:14 -0400

    Cube example added

--- src/main.js
+++ src/main.js
@@ -3,34 +3,86 @@
 var Engine = famous.core.Engine;
 var Surface = famous.core.Surface;
 var Transform = famous.core.Transform;
-var StateModifier = famous.modifiers.StateModifier;
-var SpringTransition = famous.transitions.SpringTransition;
+var Modifier = famous.core.Modifier;
+var View = famous.core.View;
 
 // create the main context
 var mainContext = Engine.createContext();
 
 mainContext.setPerspective(1000);
 
-var square = new Surface({
-  size: [200, 200],
-  classes: ['blue-background', 'backfaceVisibility']
+function Cube(options){
+  View.call(this, options);
+  this.size = options.size || 200;
+  this.initSurfaces();
+}
+
+Cube.prototype = Object.create(View.prototype);
+Cube.prototype.constructor = Cube;
+
+Cube.prototype.initSurfaces = function(){
+
+  var surfaceRotations = [
+    [0, 0, 0],
+    [0, degreesToRadian(90), 0],
+    [0, degreesToRadian(-90), 0],
+    [0, degreesToRadian(180), 0],
+    [degreesToRadian(90), 0, 0],
+    [degreesToRadian(-90), 0, 0]
+  ];
+
+  var surfaceTranslations = [
+    [0, 0, this.size/2],
+    [this.size/2, 0, 0],
+    [-this.size/2, 0, 0],
+    [0, 0, -this.size/2],
+    [0, this.size/2, 0],
+    [0, -this.size/2, 0]
+  ];
+
+  for(var i = 0; i < 6; i++){
+    var opacityModifier = new Modifier({
+      opacity: 0.8
+    });
+
+    var translationModifier = new Modifier({
+      transform: Transform.translate.apply(this, surfaceTranslations[i] )
+    });
+
+    var rotationModifier = new Modifier({
+      transform: Transform.rotate.apply(this, surfaceRotations[i])
+    });
+
+    var surface = new Surface({
+      size: [this.size, this.size],
+      properties: {
+        border: '1px solid #333'
+      },
+      classes: ['backfaceVisibility', 'blue-background']
+    });
+
+    this.add(opacityModifier).add(translationModifier).add(rotationModifier).add(surface);
+  }
+}
+
+function degreesToRadian(deg){
+  return deg * Math.PI/180;
+}
+
+var cube = new Cube({
+  size: 200
 });
 
-var stateModifier = new StateModifier({
+
+var angle = 1;
+var modifier = new Modifier({
   align: [0.5, 0.5],
-  origin: [0.5, 0.5]
+  origin: [0.5, 0.5],
+  transform: function(){
+    angle += 0.02;
+    return Transform.rotate(angle, angle, 0);
+  }
 });
 
-stateModifier.setTransform(Transform.rotateY(1000), {
-  method: SpringTransition,
-  period: 500,
-  dampingRatio: 0.2
-});
 
-stateModifier.setTransform(Transform.rotateY(0), {
-  method: SpringTransition,
-  period: 500,
-  dampingRatio: 0.2
-});
-
-mainContext.add(stateModifier).add(square);
+mainContext.add(modifier).add(cube);
