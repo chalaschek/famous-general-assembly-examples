06b4b1f2d88a3293c4117203a960e8c03644c65d
chalaschek <chalaschek@gmail.com>
2014-10-21 17:07:39 -0400

    Translate example added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 var Engine = famous.core.Engine;
 var Modifier = famous.core.Modifier;
 var Surface = famous.core.Surface;
+var Transform = famous.core.Transform;
 
 // create the main context
 var mainContext = Engine.createContext();
@@ -16,7 +17,8 @@ var square = new Surface({
 
 var modifier = new Modifier({
   align: [0.5, 0.5],
-  origin: [0.5, 0.5]
+  origin: [0.5, 0.5],
+  transform: Transform.translate(100, 200)
 });
 
 mainContext.add(modifier).add(square);
