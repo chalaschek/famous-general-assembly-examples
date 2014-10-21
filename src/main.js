b5f72ec4086a4c199b52f127b21ce7dc9cc7e775
chalaschek <chalaschek@gmail.com>
2014-10-21 17:03:10 -0400

    Shell cleanup

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 /*global famous*/
 // import dependencies
 var Engine = famous.core.Engine;
-var Modifier = famous.core.Modifier;
-var Transform = famous.core.Transform;
-var ImageSurface = famous.surfaces.ImageSurface;
 
 // create the main context
 var mainContext = Engine.createContext();
