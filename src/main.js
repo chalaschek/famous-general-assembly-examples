4f7e2be4b0da813321a92a87103a3b3dc8ce4450
chalaschek <chalaschek@gmail.com>
2014-10-21 16:57:54 -0400

    Clean shell

--- src/main.js
+++ src/main.js
@@ -7,21 +7,3 @@ var ImageSurface = famous.surfaces.ImageSurface;
 
 // create the main context
 var mainContext = Engine.createContext();
-
-// your app here
-var logo = new ImageSurface({
-    size: [200, 200],
-    content: 'http://code.famo.us/assets/famous_logo.svg',
-    classes: ['double-sided']
-});
-
-var initialTime = Date.now();
-var centerSpinModifier = new Modifier({
-    origin: [0.5, 0.5],
-    align: [0.5, 0.5],
-    transform : function () {
-        return Transform.rotateY(.002 * (Date.now() - initialTime));
-    }
-});
-
-mainContext.add(centerSpinModifier).add(logo);
